562c459036ae67a0f2d76664655e438c1d9d4b0c
Jeremiah Ojo <ojoj665@gmail.com>
2023-07-30 10:13:48 +0000

    cors

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(cors({
-      origin: '*',
-        credentials: true
+      origin: 'https://justjustin.stoplight.io',
+      credentials: true
         }));
 
 
