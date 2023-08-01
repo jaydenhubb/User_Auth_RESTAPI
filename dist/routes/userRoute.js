019f5fca8632eb743213c40243fa9bb1524b081a
Jeremiah Ojo <ojoj665@gmail.com>
2023-08-01 16:37:02 +0100

    Update userRoute.js

--- dist/routes/userRoute.js
+++ dist/routes/userRoute.js
@@ -1,9 +1,9 @@
 "use strict";
 exports.__esModule = true;
 var express_1 = require("express");
-var router = express_1["default"].Router();
 var UserController = require("../controller/userController");
 var authMiddleware_1 = require("../middlewares/authMiddleware");
+var router = express_1["default"].Router();
 router.get('/dashboard', authMiddleware_1.protect, UserController.dashboard);
 router.post('/signup', UserController.signUp);
 router.post('/login', UserController.login);
